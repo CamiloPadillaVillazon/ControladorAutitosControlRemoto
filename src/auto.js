5c84fe51918e172c26d8b56a6fe4700620e7ec6c
Camilo Padilla <camilo.padilla@ucb.edu.bo>
2024-08-31 21:13:42 -0400

    Validad Posicion Inicial

--- src/auto.js
+++ src/auto.js
@@ -1,5 +1,12 @@
 function validarSuperficie(superficie) {
-    const [x, y] = superficie.split(',').map(Number);
-    return {x, y};
+    const [xMax, yMax] = superficie.split(',').map(Number);
+    return {x: xMax, y:yMax};
 }
-export default validarSuperficie;
+
+function validarPosicionInicial(pInicial, superficie){
+    const [x, y] = pInicial.split(',').map(Number);
+    const [xMax, yMax] = superficie.split(',').map(Number);
+    return x >= 0 && x <= xMax && y >= 0 && y <= yMax
+}
+
+export {validarSuperficie, validarPosicionInicial};
--- src/auto.test.js
+++ src/auto.test.js
@@ -1,7 +1,13 @@
-import validarSuperficie from "./auto.js";
+import {validarSuperficie, validarPosicionInicial, validarDireccion} from "./auto.js";
 
 describe("Validaciones", () => {
     it("Deberia validar el tamaño de la superficie", () => {
     expect(validarSuperficie("4,5")).toEqual({x: 4, y: 5});
 });
+
+it("Deberia validar la posicion inical si esta en rango de la superficie", () => {
+    expect(validarPosicionInicial("4,5", "5,5")).toEqual(true);
+});
+
+
 });
